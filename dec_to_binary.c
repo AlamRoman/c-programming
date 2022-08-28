989ad921080dd8db88f9b252ef3ef8b587848de0
Roman <romanalam8083@gmail.com>
2022-08-29 01:34:54 +0200

    Added decimal to binary project

---
+++ dec_to_binary.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <math.h>
+#define true 1
+
+int digit_to_store_binary(int number){
+    int n=1;
+    while (true)
+    {
+        if (pow(2,n)>number)
+        {
+            return n;
+        }else
+        {
+            n++;
+        }
+        
+        
+    }
+    
+}
+
+int main(){
+
+    int n,i;
+    printf("Enter the number to convert: ");
+    scanf("%d",&n);
+    if (n==0)
+    {
+        printf("0");
+        return 0;
+    }
+    
+    int size=digit_to_store_binary(n);
+    int bin[size];
+    for ( i = 0; n >= 1; i++)
+    {
+        if (n%2==0)
+        {
+            bin[i]=0;
+            n=n/2;
+        }else
+        {
+            bin[i]=1;
+            n=floor(n/2);
+        }
+
+    }
+    
+    for ( i = size-1; i >= 0; i--)
+    {
+        printf("%d  ",bin[i]);
+    }
+    
+
+    return 0;
+}
